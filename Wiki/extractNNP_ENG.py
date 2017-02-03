928517ff010e694563d8076ca4d46bad97261480
SeoJiHyeong <21000353@handong.edu>
2017-02-03 13:52:45 +0900

    extractNNP_ENG

---
+++ Wiki/extractNNP_ENG.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from nltk.tag import pos_tag
+def extractNNP_ENG(i):
+    f = open("../../data/Wiki/sample/header/eng/"+str(i)+".txt","rU",encoding='UTF8')
+    e_list = []
+    while 1:
+        sentence = f.readline()
+        if not sentence:
+            break;
+        tagged_sent = pos_tag(sentence.split())
+        propernouns = [word for word,pos in tagged_sent if pos == 'NNP']
+        e_list.append(propernouns)
+
+    return e_list
+extractNNP_ENG(1)
