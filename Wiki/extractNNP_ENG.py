7d78f6a45a7c385eb25769cd20196873e1517875
slowcity <luinpark@naver.com>
2017-02-06 14:45:32 +0900

    Update extractNNP_ENG.py

--- Wiki/extractNNP_ENG.py
+++ Wiki/extractNNP_ENG.py
@@ -9,7 +9,7 @@ def extractNNP_ENG(i):
         if not sentence:
             break;
         tagged_sent = pos_tag(sentence.split())
-        propernouns = [word for word,pos in tagged_sent if pos == 'NNP']
+        propernouns = [word for word,pos in tagged_sent if pos == 'NNP' or pos == 'NN' or pos == 'NNS']
         e_list.append(propernouns)
 
     return e_list
