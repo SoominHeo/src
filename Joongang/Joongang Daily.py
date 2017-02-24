8193509eef927ec7931e94f7b41b707cd3238865
SeoJiHyeong <21000353@handong.edu>
2017-02-24 11:37:35 +0900

    main change

--- Joongang/Joongang Daily.py
+++ Joongang/Joongang Daily.py
@@ -43,15 +43,26 @@ def div_english_sentence(bodies_split,filenumber,csv2):
     bodies_split[0] = p.sub('',bodies_split[0])
         
         
-        
+    
     #div enlish sentence
     bodies_split[0]=bodies_split[0].replace(". \n",".**next**")
-    bodies_split[0]=bodies_split[0].replace(".\n",".**next**")
-        
+    bodies_split[0]=bodies_split[0].replace(".\n", ".**next**")
+    bodies_split[0]=bodies_split[0].replace("” \n","”**next**")
+    bodies_split[0]=bodies_split[0].replace("”\n", "”**next**")
+    bodies_split[0]=bodies_split[0].replace("＂\n","＂**next**")
+    bodies_split[0]=bodies_split[0].replace("＂\n","＂**next**")
     mun_split=bodies_split[0].split("**next**")
-        
     a = len(mun_split)
-        
+    for h in range(a):
+        flag=0
+        for y in range(len(mun_split[h])):
+            if(flag==1 and mun_split[h][y]=='.' and y+1<len(mun_split[h])):
+                if(mun_split[h][y+1]!='”' and mun_split[h][y+1]!='＂'):
+                    mun_split[h] = mun_split[h][:y] + " " + mun_split[h][y+1:]
+            if(flag==1 and (mun_split[h][y]=='”' or mun_split[h][y]=='＂')):
+                flag=0
+            elif(flag==0 and (mun_split[h][y]=='“' or mun_split[h][y]=='＂') ):
+                flag=1
     f = open("../../data/Joongang/original_text/en/"+str(filenumber)+".txt","w",encoding="UTF8")
     tmp=""
     for n in range(a):
@@ -62,7 +73,7 @@ def div_english_sentence(bodies_split,filenumber,csv2):
             tokens[m] = p.sub('',tokens[m])
             tokens[m]=tokens[m].replace(".” ",".”");
             tokens[m]=tokens[m].replace(".”",".”\n");
-                
+            '''
             p = re.compile('JoongAng Ilbo.*\s*\n*')
             tokens[m] = p.sub('',tokens[m])
             p = re.compile('The author is.*\s*\n*')
@@ -71,22 +82,25 @@ def div_english_sentence(bodies_split,filenumber,csv2):
             tokens[m] = p.sub('',tokens[m])
             p = re.compile('by .+[ ]*.*-.+')
             tokens[m] = p.sub('',tokens[m])
+            '''
             tmp=tmp+tokens[m]
             tmp=tmp+"\n"
-            
         tmp = tmp+"\n"
-        
+    '''
     tokens = tmp.split("\n\n\n")
+    
     s = len(tokens[0])
     for p in range(s):
         if tokens[0][s-1-p]=="\n":
             tokens[0] = tokens[0][0:s-1-p]
         else:
             break
+    '''
     now = time.localtime()
     s = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
     csv2.write(str(s)+"\n")
-    f.write(tokens[0])
+    f.write(tmp)
+
     f.close()
 
 
@@ -352,10 +366,10 @@ def get_html_csv():
 
 
 
-def save_content():
-    filenumber = 0
+def save_content(start,end):
+    filenumber = start
     csv2 = open("../../data/Joongang/original_text/attribute.csv","w",encoding="UTF8")
-    while 1:
+    while filenumber <= end:
         print(filenumber)
         file = open("../../data/Joongang/original_html/html/"+str(filenumber)+".html","rU",encoding='UTF8')
         data = BeautifulSoup(file,"html.parser")
@@ -392,17 +406,24 @@ def save_content():
         bodies_split=bodies.split("!div_eng_kor!")
             
         if len(bodies_split)<2:
-            x=x+1
+            #x=x+1
             filenumber = filenumber+1
             continue;
             
         div_english_sentence(bodies_split,filenumber,csv2)
         div_korean_sentence(bodies_split,filenumber,csv2)
         filenumber = filenumber + 1
+        
     csv2.close()
 
 #get_html_csv()
-#save_content()
-#number_ko.number_ko()
-#number_en.nunmber_en()
-#LCS.LCS()
+print("save_content")
+save_content(0,8217)
+'''
+print("number_ko")
+number_ko.number_ko(0,8217)
+print("number_en")
+number_en.number_en(0,8217)
+print("LCS")
+LCS.run(0,8217)
+'''
