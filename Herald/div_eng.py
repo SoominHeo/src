3e6a1a218f80d1c0306d114180a4cf8bcf089e90
SeoJiHyeong <21000353@handong.edu>
2017-02-24 17:29:26 +0900

    div_eng

---
+++ Herald/div_eng.py
@@ -0,0 +1,49 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+from unicodedata import name
+from nltk import sent_tokenize
+def div_english_sentence(content,filenumber):
+    #div enlish sentence
+    '''
+    content=content.replace(". \n",".**next**")
+    content=content.replace(".\n", ".**next**")
+    content=content.replace("” \n","”**next**")
+    content=content.replace("”\n", "”**next**")
+    content=content.replace("＂\n","＂**next**")
+    content=content.replace("＂\n","＂**next**")
+    '''
+    f = open("./div/eng/"+str(filenumber)+".txt","w",encoding="UTF8")
+    for x in content:
+        flag=0
+        x = str(x).replace(".” ",".”\n")
+        x = str(x).replace(".\" ",".\"\n")
+        for y in range(len(x)):
+            if(flag==1 and (x[y]=='.'or x[y]=='?') and y+1<len(x)):
+                if(x[y+1]!='”' and x[y+1]!='＂' and x[y+1]!=')'):
+                    if(x[y]=='?'):
+                        x=str(x).replace('?',"@#$")
+                    elif(x[y]=='.'):
+                        x=str(x).replace('.',"%^&*")
+            if(flag==1 and (x[y]=='”' or x[y]=='＂' or x[y]==')')):
+                flag=0
+            elif(flag==0 and (x[y]=='“' or x[y]=='＂' or x[y]=='(') ):
+                flag=1
+
+        tokens = sent_tokenize(x)
+        for m in range(len(tokens)):
+            tmp = str(tokens[m]).replace('@#$','?')
+            tmp = str(tmp).replace('%^&*','.')
+            f.write(tmp+"\n")
+
+    f.close()
+
+
+def div_eng(start, end):
+    for i in range(start, end):
+        f = open("./jh/eng/"+str(i)+".txt","rU",encoding="UTF8")
+        content = f.readlines()
+        div_english_sentence(content,i) 
+   
+div_eng(0,1840) 
+
