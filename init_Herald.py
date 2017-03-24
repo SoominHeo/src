c126aadffc6dc366aecc8f10d618cce3e2ee77c7
SeoJiHyeong <21000353@handong.edu>
2017-03-24 17:06:05 +0900

    Add files via upload

--- init_Herald.py
+++ init_Herald.py
@@ -6,10 +6,8 @@ data_path = "/../data/"
 wiki_path = "/../data/Herald/"
 ANS_path = "/../data/Herald/ANS/result/{subtype}/{distance}/{value}/"
 LCS_path = "/../data/Herald/LCS/{subtype}/{lang}/{distance}/{value}/"
-
-
-
-NUM_path = "/../data/wiki/NUM/{lang}/"
+NUM_path = "/../data/Herald/NUM/{lang}/"
+original_path = "/../data/Herald/resource/{lang}/"
 def make_directory(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
@@ -28,7 +26,9 @@ def check_directory(subtype, distance,value):
 	make_directory(current_path+NUM_path.format(lang="eng"))
 	make_directory(current_path+NUM_path.format(lang="kor"))
 
+	make_directory(current_path+original_path.format(lang="eng"))
+	make_directory(current_path+original_path.format(lang="kor"))
 check_directory("word_fill","5","0.3")
-zip_ref = zipfile.ZipFile("./sample.zip", 'r')
-zip_ref.extractall("../data/")
+zip_ref = zipfile.ZipFile("./resource.zip", 'r')
+zip_ref.extractall("../data/Herald/")
 zip_ref.close()
