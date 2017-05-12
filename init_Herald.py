49e4fc9c47152c86a159968397815b8fa2bcf965
SeoJiHyeong <21000353@handong.edu>
2017-05-12 17:31:33 +0900

    Update init_Herald.py

--- init_Herald.py
+++ init_Herald.py
@@ -8,6 +8,7 @@ ANS_path = "/../data/Herald/ANS/result/{subtype}/{distance}/{value}/"
 LCS_path = "/../data/Herald/LCS/{subtype}/{lang}/{distance}/{value}/"
 NUM_path = "/../data/Herald/NUM/{lang}/"
 original_path = "/../data/Herald/resource/{lang}/"
+sample_path = "/../data/Herald/sample/{lang}/"
 def make_directory(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
@@ -28,6 +29,8 @@ def check_directory(subtype, distance,value):
 
 	make_directory(current_path+original_path.format(lang="eng"))
 	make_directory(current_path+original_path.format(lang="kor"))
+	make_dicrectory(current_path+sample_path.format(lang="eng")
+	make_dicrectory(current_path+sample_path.format(lang="kor"))
 check_directory("word_fill","5","0.3")
 zip_ref = zipfile.ZipFile("./resource.zip", 'r')
 zip_ref.extractall("../data/Herald/")
